1cac6b1efa84283811c05ec8555b5f4a8f6d7077
Ahasil <lazyahasil+dev@gmail.com>
2018-11-20 17:33:00 +0900

    Added func "GET_MATE_P_PID" and edited "SPEAR_LINE_ROOT_OF_FA"
    The PL/SQL function `SPEAR_LINE_ROOT_OF_FA` had used to return `NULL` when the starting human is the root of him or her.

--- sql-files/plsql-ahasil.sql
+++ sql-files/plsql-ahasil.sql
@@ -1,4 +1,23 @@
-/* 친가 부계의 root가 속한 mate PID 찾기 */
+/* 자신의 Mate_P PID 한 개 찾기 */
+CREATE OR REPLACE FUNCTION GET_MATE_P_PID (
+        TARGET_HID IN HUMAN.HID%TYPE
+    )
+    RETURN PARENTS.PID%TYPE
+IS
+    CURSOR cur IS SELECT P.PID
+        FROM HUMAN H, PARENTS P
+        WHERE H.HID = TARGET_HID AND (H.HID = P.FHID OR H.HID = P.MHID);
+    mate_p_pid PARENTS.PID%TYPE;
+BEGIN
+    mate_p_pid := NULL;
+    OPEN cur;
+        FETCH cur INTO mate_p_pid;
+    CLOSE cur;
+    RETURN mate_p_pid;
+END;
+/
+
+/* 친가 부계의 root가 속한 Mate_P PID 찾기 */
 CREATE OR REPLACE FUNCTION SPEAR_LINE_ROOT_OF_FA (
         START_HID IN HUMAN.HID%TYPE
     )
@@ -11,21 +30,26 @@ IS
         ORDER BY LEVEL DESC;
     temp_level NUMBER;
     temp_hid HUMAN.HID%TYPE;
-    root_pid PARENTS.PID%TYPE;
+    root_mate_p_pid PARENTS.PID%TYPE;
     temp_fhid HUMAN.HID%TYPE;
     temp_mhid HUMAN.HID%TYPE;
 BEGIN
-    root_pid := NULL;
+    root_mate_p_pid := NULL;
     OPEN cur;
-        FETCH cur INTO temp_level, temp_hid, root_pid, temp_fhid, temp_mhid;
+        /* ROOT ROW */
+        FETCH cur INTO temp_level, temp_hid, root_mate_p_pid, temp_fhid, temp_mhid;
         IF NOT cur%NOTFOUND THEN
-            FETCH cur INTO temp_level, temp_hid, root_pid, temp_fhid, temp_mhid;
+            /* UNDER-ROOT ROW: PID is MATE_P PID of ROOT! */
+            FETCH cur INTO temp_level, temp_hid, root_mate_p_pid, temp_fhid, temp_mhid;
+        ELSE
+            /* Only one row exists */
+            root_mate_p_pid := GET_MATE_P_PID(temp_hid);
         END IF;
     CLOSE cur;
-    RETURN root_pid;
+    RETURN root_mate_p_pid;
 END;
 /
 
-SELECT HID, SPEAR_LINE_ROOT_OF_FA(HID) AS ROOT_MATE_P_PID
+SELECT HID, NAME, SPEAR_LINE_ROOT_OF_FA(HID) AS ROOT_MATE_P_PID, GET_MATE_P_PID(HID)
     FROM HUMAN
-    WHERE NAME = '여자';
+    ORDER BY BIRTH;
