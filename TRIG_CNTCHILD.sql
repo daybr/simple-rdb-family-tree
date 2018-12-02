1a54959189c85cb933b4111c7203659c6f433542
k03147 <31230483+k03147@users.noreply.github.com>
2018-12-02 14:09:40 +0900

    add TRIG_CNTCHILD.sql

---
+++ TRIG_CNTCHILD.sql
@@ -0,0 +1,6 @@
+create or replace trigger trig_cntchild
+after
+update or insert on human
+begin
+  update parents p set p.childcount = (select COUNT(h.hid) from human h where h.pid = p.pid);
+end trig_cntchild;
